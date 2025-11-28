136eca605adf933e4bc5c3a7acccb7e9eb0e8e92
Cristian Alexandru <cristian.alexandru@spark-radiance.com>
2025-11-28 23:40:41 +0200

    fix

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<VerbCacheService>();
+builder.Services.AddSingleton<VerbCacheService>();
 
 var app = builder.Build();
 
